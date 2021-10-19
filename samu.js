dd6aef79947478b3b777437654607c781a91de9d
Jhoel <88072905+JYBriggitt@users.noreply.github.com>
2021-10-19 13:56:00 -0500

    update samu.js

--- samu.js
+++ samu.js
@@ -2363,7 +2363,7 @@ addFilter(from)
 aud = fs.readFileSync('./audio/asen.ogg') 
 samu330.sendMessage(from, aud, audio, {quoted: faud, mimetype: 'audio/mp4', ptt: true, duration: -999999, sendEphemeral: true}) 
 break
- 'hentai':
+ caseb'hentai':
 addFilter(from)
 aud = fs.readFileSync('./audio/hentai.ogg') 
 samu330.sendMessage(from, aud, audio, {quoted: faud, mimetype: 'audio/mp4', ptt: true, duration: -999999, sendEphemeral: true}) 
