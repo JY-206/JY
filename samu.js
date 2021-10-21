12eb53b762ead95e0434706ff9a8efd5a1d7c4c3
Jhoel <88072905+JYBriggitt@users.noreply.github.com>
2021-10-21 09:27:01 -0500

    update samu.js

--- samu.js
+++ samu.js
@@ -3510,7 +3510,7 @@ case 'play':
 	assistant = fs.readFileSync('./src/assistant.jpg')		
 	if (!isRegister) return samu330.sendMessage(from, assistant, image, { quoted: noreg, caption: `😊 ${timeFt}.\n*Yo soy Sam330*, Asistente de *JY*!.\n\nAl parecer no estas registrado en _*Bot*_, Para registrarte usa el comando: *${prefix}reg*.`, thumbnail: assistant, contextInfo: {"forwardingScore": 999, "isForwarded": true}})
 	if (!q) return reply('*Que audio quieres descargar?.....*')
-	let plist = await yts(q)
+	
 	sendFileFromUrl(plist.all[0].image, image, {quoted: sam, caption: '_*Si no ves la lista de descarga de tu audio, prueba usando el comando play2*_'})
 
 	let play2v = samu330.prepareMessageFromContent(from,{
