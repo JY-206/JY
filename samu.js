8e9eddfb2d504d51bf9af596dc5858f93b3f6342
Jhoel <88072905+JYBriggitt@users.noreply.github.com>
2021-11-10 14:20:06 -0500

    update samu.js

--- samu.js
+++ samu.js
@@ -1542,7 +1542,6 @@ ${samu}◦ 🌐Navegador :${samu} *${samu330.browserDescription[1]}*
 ${samu}◦ 📡servidor :${samu} *${samu330.browserDescription[0]}*
 ${samu}◦ ✅version :${samu} *${samu330.browserDescription[2]}*
 ${samu}◦ 🚄Velocidad :${samu} *${process.uptime()}*
-${samu}◦ 📲Sistema operativo :${samu} *${samu330.user.phone.device_manufacturer}*
 ${samu}◦ 🪀version de${samu} *WhatsApp* : *${samu330.user.phone.wa_version}*
 
 ༶•┈┈⛧┈♛ ♛┈⛧┈┈•༶
